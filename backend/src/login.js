ecc694a71ad10935a2677e9275fcf76d70d8f7d9
Rene Speck <rene.speck@web.de>
2012-11-19 15:32:29 +0100

    added create user function

--- backend/src/login.js
+++ backend/src/login.js
@@ -4,6 +4,26 @@ var crypto = require('crypto');
 var Login = exports.Login = function (database) {
     this.db = database;
     this.blob = '"§$%&/()(/&%$'; // md5 blob
+    /*insert test data*/
+    var user = {
+        email: 'foo@bar.de',
+        password: crypto.createHash('md5').update('foobar' + this.blob).digest('hex'),
+        name: "foobar",
+        code: ""
+    };
+    this.createUser(user, function (err, res) { });
+    /**/
+};
+
+Login.prototype.createUser = function (user, callback) {
+    this._collection(callback, function (err, coll) {
+        coll.insert(user, function (err, res) {
+            if (err) {
+                callback(err);
+            }
+            callback(null, res);
+        });
+    });
 };
 
 Login.prototype._collection = function (errCallback, callback) {
@@ -15,9 +35,9 @@ Login.prototype._collection = function (errCallback, callback) {
     });
 };
 
-Login.prototype.list = function (login, callback) {
+Login.prototype.list = function (email, password, callback) {
     this._collection(callback, function (err, coll) {
-        var cursor = coll.find({ email: login.email, password: login.password });
+        var cursor = coll.find({ email: email, password: password });
         cursor.toArray(function (err, res) {
             if (err) {
                 callback(err);
@@ -28,19 +48,14 @@ Login.prototype.list = function (login, callback) {
 };
 
 Login.prototype.standard = function (email, password, callback) {
-
-    var login = {
-        email: email,
-        password: crypto.createHash('md5').update(password + this.blob).digest('hex')
-    };
-
-    this.list(login, function (err, result) {
+    password = crypto.createHash('md5').update(password + this.blob).digest('hex')
+    this.list(email, password, function (err, result) {
         if (err)
             callback(err);
 
-        if (result.length >=1) // TODO
-            callback(null, true);
+        if (result.length >= 1) // TODO
+            callback(null, true, result);
         else
-            callback(null, false);
+            callback(null, false, null);
     });
 };
