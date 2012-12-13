205b49694549a2bfa2e774148e2bc71851beb143
Rene_Speck <rene.speck@web.de>
2012-12-13 15:02:14 +0100

    added schema for user

--- backend/src/login.js
+++ backend/src/login.js
@@ -1,15 +1,28 @@
 ﻿var util = require('util');
 var crypto = require('crypto');
+var schemajs = require("schemajs");
+
+// user schema for login
+var schema = {
+    user: {
+        type: "object", required: true,
+        schema: {
+            email: { type: "string+", required: true },
+            password: { type: "string+", required: true },
+            name: { type: "string+", required: true }
+        }
+    }
+};
+//util.puts(schemajs.create(schema).validate({ user: { email: "test", password: "test", name: "test"} }).valid); // true
 
 var Login = exports.Login = function (database) {
     this.db = database;
     this.blob = '"§$%&/()(/&%$'; // md5 blob
-    /*insert test data*/
+    /*insert test data
     var user = {
         email: 'foo@bar.de',
         password: crypto.createHash('md5').update('foobar' + this.blob).digest('hex'),
-        name: "foobar",
-        code: ""
+        name: "foobar"
     };
     this.createUser(user, function (err, res) { });
     /**/
@@ -17,11 +30,17 @@ var Login = exports.Login = function (database) {
 
 Login.prototype.createUser = function (user, callback) {
     this._collection(callback, function (err, coll) {
-        coll.insert(user, function (err, res) {
-            if (err) {
-                callback(err);
+        coll.find({ email: user.email }).toArray(function (err, docs) {
+            if (docs.length == 0) {
+                coll.insert(user, function (err, res) {
+                    if (err) {
+                        callback(err);
+                    }
+                    callback(null, res);
+                });
+            } else {
+                //TODO more than one user
             }
-            callback(null, res);
         });
     });
 };
