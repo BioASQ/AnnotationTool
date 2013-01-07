fb88fc8c293cb41bb8e36f8188891e7f06d6d675
Norman Heino <norman.heino@gmail.com>
2013-01-07 13:44:44 +0100

    Add remaining search services.

--- backend/src/search.js
+++ backend/src/search.js
@@ -3,7 +3,9 @@ var
   step = require('step');
 
 var Search = exports.Search = function () {
+  this.doid = new TIService('http://www.gopubmed.org/web/bioasq/doid/json');
   this.go = new TIService('http://www.gopubmed.org/web/bioasq/go/json');
+  this.jochem = new TIService('http://www.gopubmed.org/web/bioasq/jochem/json');
   this.mesh = new TIService('http://www.gopubmed.org/web/bioasq/mesh/json');
   this.uniprot = new TIService('http://www.gopubmed.org/web/bioasq/uniprot/json');
 };
@@ -20,16 +22,18 @@ Search.prototype.find = function (/* String */ keywords, cb) {
   var self = this;
   step(
     function () {
+      self.doid.find(keywords, this.parallel()),
       self.go.find(keywords, this.parallel()),
+      self.jochem.find(keywords, this.parallel()),
       self.mesh.find(keywords, this.parallel()),
       self.uniprot.find(keywords, this.parallel())
     },
-    function (err, goRes, meshRes, uniprotRes) {
+    function (err, doidRes, goRes, jochemRes, meshRes, uniprotRes) {
       if (err) {
         cb(err);
       } else {
         // merge the 'findings' section of each result and return
-        cb(null, self._merge('findings', goRes, meshRes, uniprotRes));
+        cb(null, self._merge('findings', doidRes, goRes, jochemRes, meshRes, uniprotRes));
       }
     }
   );
