612df58fb7671eec502def7b177d92025d589b9e
Rene Speck <rene.speck@web.de>
2013-01-08 18:12:23 +0100

    add winston logging

---
+++ backend/src/logging.js
@@ -0,0 +1,47 @@
+﻿var winston = require('winston');
+var mongoDB = require('winston-mongodb').MongoDB;
+var path = require('path');
+var config = require(path.join(__dirname, '..', 'config')).defaults;
+
+var singleton = function singleton() {
+
+    this.logger = new (winston.Logger)({
+        transports: [
+          new (winston.transports.Console)({
+              level: config.logging.level,
+          }),
+
+          new winston.transports.MongoDB({
+              host: config.database.host,
+              port: config.database.port,
+              db: config.database.name,
+              level: config.logging.level,
+              silent: false // Where is it?
+          })
+        ],
+
+        exceptionHandlers: [
+            new winston.transports.File({
+                filename: path.join(__dirname, '..', '..', config.logging.exceptionsFile)
+            })
+        ]
+    });
+
+    if (singleton.caller != singleton.getInstance) {
+        throw new Error("This object cannot be instanciated");
+    }
+}
+
+singleton.instance = null;
+
+singleton.getInstance = function () {
+    if (this.instance === null)
+        this.instance = new singleton();
+    return this.instance;
+}
+
+module.exports = singleton.getInstance();
+
+//
+// var logger = new require('./logging.js').logger;
+// logger.info(logger === logger2);
