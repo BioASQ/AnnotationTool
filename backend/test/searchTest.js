3fbe673729bc5b2179643d70a2064f747a274bc4
Norman Heino <norman.heino@gmail.com>
2013-01-11 13:05:40 +0100

    Fix search test.

--- backend/test/searchTest.js
+++ backend/test/searchTest.js
@@ -1,11 +1,11 @@
 var
-  Search = require('../src/ticoncepts').Search,
+  Search = require('../src/search').Search,
   assert = require('assert'),
   util = require('util');
 
 var s = new Search();
 
-s.find('foxp1', function (err, res) {
+s.find('diabetes', function (err, res) {
   console.log(res);
   assert.ifError(err);
   process.exit(0);
