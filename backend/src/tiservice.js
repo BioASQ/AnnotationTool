264b36aa5d2684d6b87999cd07738349a1b2e96a
Norman Heino <norman.heino@gmail.com>
2013-01-25 22:51:26 +0100

    Add new method that parses response as JSON.

--- backend/src/tiservice.js
+++ backend/src/tiservice.js
@@ -1,88 +1,101 @@
-var
-  http = require('http'),
-  url = require('url'),
-  qstr = require('querystring');
+var http = require('http'),
+    url = require('url'),
+    qstr = require('querystring');
 
 var TIService = exports.TIService = function (URL) {
-  this.serviceURL = URL;
-  this.timeout = 600000; // 10 min in ms
-  this.currentTimeout = null;
-  this.tokenURL = null;
+    this.serviceURL = URL;
+    this.timeout = 600000; // 10 min in ms
+    this.currentTimeout = null;
+    this.tokenURL = null;
 };
 
 /*
  * Retrieves the token URL from the server
  */
 TIService.prototype._tokenURL = function (cb) {
-  var self = this;
-  if (this.tokenURL) {
-    cb(this.tokenURL);
-    return;
-  }
-  // token invalid
-  this._request(this.serviceURL, { 'method': 'GET' },
-      function (err, data) {
-        self.tokenURL = data;
-        cb(self.tokenURL);
-      }
-    );
+    var self = this;
+    if (this.tokenURL) {
+        return cb(this.tokenURL);
+    }
+    // token invalid
+    this._request(this.serviceURL, { 'method': 'GET' },
+            function (err, data) {
+                self.tokenURL = data;
+                cb(self.tokenURL);
+            }
+        );
 };
 
 /*
  * Basic request wrapper function
  */
 TIService.prototype._request = function (URL, options, /* Object */ data, cb) {
-  /*
-   * If cb is undefined we have been given only three parameters,
-   * the last of which is the callback.
-   */
-  cb = cb || data;
+    /*
+     * If cb is undefined we have been given only three parameters,
+     * the last of which is the callback.
+     */
+    cb = cb || data;
 
-  this._resetTimeout();
-  var urlObj = url.parse(URL);
-  var dataStr = '';
-  var httpOptions = {
-    'hostname': urlObj.host,
-    'port': urlObj.port ? urlObj.port : 80,
-    'path': urlObj.path,
-    'method': options.method ? options.method : 'POST',
-    'headers': {
-      'content-type': 'application/x-www-form-urlencoded',
-      'accept': 'application/json',
+    this._resetTimeout();
+    var urlObj = url.parse(URL);
+    var dataStr = '';
+    var httpOptions = {
+        'hostname': urlObj.host,
+        'port': urlObj.port ? urlObj.port : 80,
+        'path': urlObj.path,
+        'method': options.method ? options.method : 'POST',
+        'headers': {
+            'content-type': 'application/x-www-form-urlencoded',
+            'accept': 'application/json',
+        }
     }
-  }
-  if (data) {
-    dataStr = 'json=' + encodeURIComponent(JSON.stringify(data) + '\n');
-    httpOptions.headers['content-length'] = dataStr.length;
-  }
-  var responseData = '';
-  var req = http.request(httpOptions, function (res) {
-     res.on('data', function (chunk) {
-       responseData += chunk;
-     }).on('end', function () {
-       cb(null, responseData);
-     }).on('close', function () {
-       res.emit('end');
-     });
-  });
-  req.on('error', function (e) {
-    cb(e.message);
-  });
-  req.write(dataStr);
-  req.end();
+    if (data) {
+        dataStr = 'json=' + encodeURIComponent(JSON.stringify(data) + '\n');
+        httpOptions.headers['content-length'] = dataStr.length;
+    }
+    var responseData = '';
+    var req = http.request(httpOptions, function (res) {
+         res.on('data', function (chunk) {
+             responseData += chunk;
+         }).on('end', function () {
+             if (responseData.exception) {
+                 cb(responseData.exception);
+             } else {
+                 cb(null, responseData);
+             }
+         }).on('close', function () {
+             res.emit('end');
+         });
+    });
+    req.on('error', function (e) {
+        cb(e.message);
+    });
+    req.write(dataStr);
+    req.end();
 };
+
+TIService.prototype._requestJSON = function (URL, options, /* Object */ data, cb) {
+    this._request(URL, options, data, function (err, data) {
+        if (err) { return cb(err); }
+
+        var response = JSON.parse(data);
+        if (response.exception) {
+            return cb(Error(response.exception));
+        }
+        cb(null, response);
+    });
+}
 
 /*
  * Clears the current timeout (if any) and creates a new one.
  */
 TIService.prototype._resetTimeout = function () {
-  var self = this;
-  if (this.currentTimeout) {
-    clearTimeout(this.currentTimeout);
-  }
-  this.currentTimeout = setTimeout(function () {
-    self.tokenURL = null;
-  }, this.timeout);
+    var self = this;
+    if (this.currentTimeout) {
+        clearTimeout(this.currentTimeout);
+    }
+    this.currentTimeout = setTimeout(function () {
+        self.tokenURL = null;
+    }, this.timeout);
 };
-
 
