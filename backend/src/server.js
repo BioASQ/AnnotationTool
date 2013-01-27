c1e1e928dfb1214e16955cb8e97b156d4a31eada
Norman Heino <norman.heino@gmail.com>
2013-01-27 14:52:59 +0100

    Add authentication check for static files.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,72 +1,78 @@
-var
-  http = require('http'),
-  util = require('util'),
-  url = require('url'),
-  mongodb = require('mongodb'),
-  send = require('send'),
-  path = require('path');
+var http = require('http'),
+util = require('util'),
+url = require('url'),
+mongodb = require('mongodb'),
+send = require('send'),
+path = require('path');
 
 var _setHeader = http.ServerResponse.prototype.setHeader;
 var session = require('sesh').session;
 // undo response's monkey patching madness
 http.ServerResponse.prototype.setHeader = _setHeader;
 
-var
-  login = require('./login'),
-  question = require('./question');
+var login = require('./login'),
+question = require('./question');
 
-var
-  corsHeaderName = 'Access-Control-Allow-Origin';
+var corsHeaderName = 'Access-Control-Allow-Origin';
 
 exports.createServer = function (port, model, authentication) {
-  var server = http.createServer(function (request, response) {
-    var body = '';
-    request.on('data', function (chunk) {
-      body += chunk;
-    });
-    request.on('end', function () {
-      var parsedURL = url.parse(request.url);
-
-      if (parsedURL.pathname.search(/backend/) > -1) {
-        session(request, response, function (request, response) {
-          var router = require('./router').createRouter(model, authentication);
-          var emitter = router.handle(request, body, function (route) {
-            var headers = route.headers;
-            // aleays inject CORS header
-            headers[corsHeaderName] = 'http://' + request.headers.host;
-
-            // send response
-            response.writeHead(route.status, headers);
-            response.end(route.body);
-          });
+    var server = http.createServer(function (request, response) {
+        var body = '';
+        request.on('data', function (chunk) {
+            body += chunk;
         });
-      } else {
-        send(request, parsedURL.pathname)
-          .root(path.join(__dirname, '..', '..', 'frontend'))
-          .pipe(response);
-      }
+        request.on('end', function () {
+            var parsedURL = url.parse(request.url);
+
+            session(request, response, function (request, response) {
+                if (parsedURL.pathname.search(/backend/) > -1) {
+                    var router = require('./router').createRouter(model, authentication);
+                    var emitter = router.handle(request, body, function (route) {
+                        var headers = route.headers;
+                        // aleays inject CORS header
+                        headers[corsHeaderName] = 'http://' + request.headers.host;
+
+                        // send response
+                        response.writeHead(route.status, headers);
+                        response.end(route.body);
+                    });
+                } else {
+                    if (request.session.data.user === 'Guest' && !isIndexRequest(parsedURL.pathname) ) {
+                        response.writeHead(403, 'Not authorized');
+                        response.write('Not authorized');
+                        response.end();
+                    } else {
+                        send(request, parsedURL.pathname)
+                            .root(path.join(__dirname, '..', '..', 'frontend'))
+                            .pipe(response);
+                    }
+                }
+            });
+        });
     });
-  });
 
-  if (port) {
-    server.listen(port);
-  }
+    if (port) {
+        server.listen(port);
+    }
 
-  return server;
+    return server;
 }
 
 exports.start = function (options, callback) {
-  var
-    dbServer = new mongodb.Server(options.database.host, options.database.port, {}),
+    var dbServer = new mongodb.Server(options.database.host, options.database.port, {}),
     dbConn = new mongodb.Db(options.database.name, dbServer, { safe: false });
 
-  dbConn.open(function (err, database) {
-    if (err) {
-      return callback({'message': 'Could not connect to database server.'});
-    }
+    dbConn.open(function (err, database) {
+        if (err) {
+            return callback({'message': 'Could not connect to database server.'});
+        }
 
-    var server = exports.createServer(options.port, new question.Question(database), new login.Login(database));
+        var server = exports.createServer(options.port, new question.Question(database), new login.Login(database));
 
-    callback(null, server);
-  });
+        callback(null, server);
+    });
+}
+
+function isIndexRequest(pathName) {
+    return (pathName.search(/^$|^\/$|index\.html$|\.css$|\.js$|\.png$/) > -1);
 }
