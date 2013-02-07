3907d62fe12a45bae61d28d475e05549bbd28293
Norman Heino <norman.heino@gmail.com>
2013-02-07 11:24:17 +0100

    Fix indent.

--- backend/lib/ticoncepts.js
+++ backend/lib/ticoncepts.js
@@ -1,8 +1,8 @@
 var
-  TIService = require('./tiservice').TIService;
+    TIService = require('./tiservice').TIService;
 
 var TIConcepts = exports.TIConcepts = function (URL) {
-  TIService.call(this, URL);
+    TIService.call(this, URL);
 };
 
 /*
@@ -14,23 +14,23 @@ TIConcepts.prototype = Object.create(TIService.prototype);
  * Override find from concepts service
  */
 TIConcepts.prototype.find = function (/* String */ keywords, cb) {
-  var self = this;
-  this._tokenURL(function (URL) {
-    self._request(URL,{ 'method': 'POST' }, { 'findEntities': [ keywords ]},
-      function (err, response) {
-        if (err) {
-          cb(err);
-        } else {
-          try {
-            var result = JSON.parse(response).result;
-            cb(null, result);
-          } catch (e) {
-            console.log('Could not parse response: ' + response);
-            cb(e);
-          }
-        }
-      }
-    );
-  });
+    var self = this;
+    this._tokenURL(function (URL) {
+        self._request(URL,{ 'method': 'POST' }, { 'findEntities': [ keywords ]},
+            function (err, response) {
+                if (err) {
+                    cb(err);
+                } else {
+                    try {
+                        var result = JSON.parse(response).result;
+                        cb(null, result);
+                    } catch (e) {
+                        console.log('Could not parse response: ' + response);
+                        cb(e);
+                    }
+                }
+            }
+        );
+    });
 };
 
