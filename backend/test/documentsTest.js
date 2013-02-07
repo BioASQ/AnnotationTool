7af82cdf39dcd07e77806f198323513b785fb163
Norman Heino <norman.heino@gmail.com>
2013-02-07 17:21:55 +0100

    Update test with integrated service URI.

--- backend/test/documentsTest.js
+++ backend/test/documentsTest.js
@@ -3,10 +3,12 @@ var
   assert = require('assert'),
   util = require('util');
 
-var s = new TIDocuments('http://www.gopubmed.com/web/bioasq/pmc/json');
+var s = new TIDocuments('http://www.gopubmed.org/web/gopubmedbeta/bioasq/pubmed');
 
 s.find('diabetes', 0, 10, function (err, res) {
-  console.log(res);
+  res.forEach(function (entry) {
+      console.log('- ' + entry.title + ' (' + entry.uri + ')');
+  })
   assert.ifError(err);
   process.exit(0);
 });
