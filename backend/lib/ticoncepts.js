25218496e6d8c40f646ad28deaf7263a714af38c
Norman Heino <norman.heino@gmail.com>
2013-02-07 17:20:30 +0100

    Use inherited _requestJSON method.

--- backend/lib/ticoncepts.js
+++ backend/lib/ticoncepts.js
@@ -16,19 +16,10 @@ TIConcepts.prototype = Object.create(TIService.prototype);
 TIConcepts.prototype.find = function (/* String */ keywords, cb) {
     var self = this;
     this._tokenURL(function (URL) {
-        self._request(URL,{ 'method': 'POST' }, { 'findEntities': [ keywords ]},
+        self._requestJSON(URL, { 'method': 'POST' }, { 'findEntities': [ keywords ]},
             function (err, response) {
-                if (err) {
-                    cb(err);
-                } else {
-                    try {
-                        var result = JSON.parse(response).result;
-                        cb(null, result);
-                    } catch (e) {
-                        console.log('Could not parse response: ' + response);
-                        cb(e);
-                    }
-                }
+                if (err) { return cb(err); }
+                cb(null, response.result);
             }
         );
     });
