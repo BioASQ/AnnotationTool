a9763d5382e2470d162bc0ef700d2c686e52f808
Norman Heino <norman.heino@gmail.com>
2013-02-11 12:51:44 +0100

    Add editing and deleting of questions.
    Fixes #71, fixes 74.

--- frontend/js/createQuestion.js
+++ frontend/js/createQuestion.js
@@ -6,6 +6,8 @@ require(["app"], function (app) {
     source = $("#questionTemplate").html();
     questionTemplate = Handlebars.compile(source);
 
+    var editTemplate = Handlebars.compile($('#editQuestionTemplate').html());
+
     // cache dom
     var questionList = $("#question"),
         newQuestionLabel = $("#newQuestionLabel"),
@@ -14,21 +16,26 @@ require(["app"], function (app) {
 
 
 
-    // fetch question list on load
-    $.getJSON(app.data.LogicServer+'questions', function(data){
-        var i, question,
-            html = "";
-        for(i = 0; i < data.questions.length; i++){
-            question = data.questions[i];
-            if( typeof question == 'undefined' ) continue;
+    var fetchQuestions = function () {
+        // fetch question list on load
+        $.getJSON(app.data.LogicServer+'questions', function(data){
+            var i, question,
+                html = "";
+            for(i = 0; i < data.questions.length; i++){
+                question = data.questions[i];
+                if( typeof question == 'undefined' ) continue;
 
-            // render to string
-            html += questionTemplate(question);
-        }
+                // render to string
+                html += questionTemplate(question);
+            }
 
-        // append to dom
-        questionList.html(html);
-    });
+            // append to dom
+            questionList.html(html);
+        });
+    }
+
+    fetchQuestions();
+
     // bind create question stuff
     $("#newQuestionCancel").click(function(){
         newQuestionLabel.val("");
@@ -74,5 +81,55 @@ require(["app"], function (app) {
 
             window.location = 'search.html';
         });
+    });
+
+    $('#editButton').click(function () {
+        var questionID = questionList.val();
+        $.getJSON(app.data.LogicServer + 'questions/' + questionID, function (data) {
+            questionTypes = [
+                { name: 'decisive', label: 'Yes/No', active: data.type == 'decisive' },
+                { name: 'factoid', label: 'Factoid', active: data.type == 'factoid' },
+                { name: 'list', label: 'List', active: data.type == 'list' },
+                { name: 'summary', label: 'Summary', active: data.type == 'summary' },
+            ];
+
+            var html = editTemplate($.extend({}, data, { questionTypes: questionTypes }));
+            $('#editQuestionModal').html(html);
+            $('#editQuestionModal').modal();
+        });
+    });
+
+    $('#editQuestionOK').live('click', function () {
+        var label      = $('#editQuestionLabel').val(),
+            type       = $("#editQuestionType").val(),
+            question   = { body: label, type: type },
+            questionID = questionList.val();
+
+        // do post request
+        $.ajax({
+            url: app.data.LogicServer + 'questions/' + questionID,
+            contentType: 'application/json',
+            dataType: 'json',
+            data: JSON.stringify(question),
+            type: 'POST',
+            success: function (data) { $('#editQuestionModal').modal('hide'); },
+            error: function () { alert('Could not update question.'); }
+        });
+    });
+
+    $('#editQuestionDelete').live('click', function () {
+        if (confirm('Are you sure?')) {
+            var questionID = questionList.val();
+            // do delete request
+            $.ajax({
+                url: app.data.LogicServer + 'questions/' + questionID,
+                type: 'DELETE',
+                success: function () {
+                    fetchQuestions();
+                    $('#editQuestionModal').modal('hide');
+                },
+                error: function () { alert('Could not delete question.'); }
+            });
+        }
     });
 });
