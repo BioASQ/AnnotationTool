d2cea3dff0f7b8bb6425d3727d0fdc4cc02fc363
Norman Heino <norman.heino@gmail.com>
2013-10-26 16:35:48 +0200

    Fix registration

--- app/js/controllers/login.js
+++ app/js/controllers/login.js
@@ -1,5 +1,5 @@
 angular.module('bioasq-at.controllers.login', [])
-.controller('LoginCtrl', function ($scope, $http, $location) {
+.controller('LoginCtrl', function ($scope, $http, $location, $modal) {
     $scope.login = function () {
         $scope.messages = [];
         if (!$scope.email) {
@@ -58,5 +58,53 @@ angular.module('bioasq-at.controllers.login', [])
                 text: 'Could not send reset email.'
             });
         });
+    };
+
+    $scope.register = function () {
+        $scope.account = {};
+        $modal.open({
+            templateUrl: '/views/partials/register.html',
+            scope: $scope
+        });
+    };
+
+    $scope.createAccount = function (form) {
+        $scope.registrationMessages = [];
+        if (form.$valid) {
+            if ($scope.account.password1 !== $scope.account.password2) {
+                $scope.registrationMessages.push({
+                    type: 'error',
+                    text: 'Passwords must match!'
+                });
+                return;
+            }
+            var regExp = new RegExp('^.*(?=.{8,})(?=.*[\\d])(?=.*[\\W]).*$', 'g');
+            if (regExp.test($scope.account.password1) === false) {
+                $scope.registrationMessages.push({
+                    type: 'error',
+                    text: 'Password does not meet criteria. Must be at least 8 characters long, 1 digit and 1 symbol'
+                });
+                return;
+            }
+            $http.post('/backend/register', {
+                email: $scope.account.email,
+                name:  $scope.account.name,
+                password: $scope.account.password1
+            })
+            .success(function () {
+
+            })
+            .error(function (data, status) {
+                $scope.registrationMessages.push({
+                    type: 'error',
+                    text: data
+                });
+            });
+        } else {
+            $scope.registrationMessages.push({
+                type: 'error',
+                text: 'Please fill in all fields.'
+            });
+        }
     };
 });
