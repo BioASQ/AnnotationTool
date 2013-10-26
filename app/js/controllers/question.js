3ba510adfbb5342328a4f8a7190903b95c3a1dd9
Norman Heino <norman.heino@gmail.com>
2013-10-26 12:24:33 +0200

    Identify questions by ID, not by $index

--- app/js/controllers/question.js
+++ app/js/controllers/question.js
@@ -9,26 +9,43 @@ angular.module('bioasq-at.controllers.question', ['bioasq-at.services.question']
         }
     });
 
+    if (!$scope.questions) {
+        $http.get('/backend/questions').then(function (result) {
+            $scope.questions = result.data.questions;
+        });
+    }
+
     $scope.question = Questions.selectedQuestion();
-    $http.get('/backend/questions').then(function (result) {
-        $scope.questions = result.data.questions;
-    });
+
     $scope.editQuestion = function (question) {
+        if (question.finalized) {
+            if (!confirm('Question is finalized. Do you want to unfinalize it?')) {
+                return;
+            }
+            $scope.question.finalized = false;
+        }
         $scope.question._body = $scope.question.body;
         $scope.question._type = $scope.question.type;
         $scope.editing  = true;
         $scope.creating = false;
     };
-    $scope.selectQuestion = function (index) {
-        $scope.question = $scope.questions[index];
-        Questions.select($scope.question);
+
+    $scope.selectQuestion = function (question) {
+        for (var i = 0; i < $scope.questions.length; i++) {
+            if ($scope.questions[i]._id === question._id) {
+                $scope.question = $scope.questions[i];
+                Questions.select($scope.question);
+            }
+        }
     };
+
     $scope.newQuestion = function () {
         $scope.questions.push({ version: 2 });
         $scope.editing  = true;
         $scope.creating = true;
         $scope.question = $scope.questions[$scope.questions.length - 1];
     };
+
     $scope.cancelEditing = function () {
         if ($scope.creating) {
             $scope.questions.pop();
@@ -40,31 +57,39 @@ angular.module('bioasq-at.controllers.question', ['bioasq-at.services.question']
         $scope.editing  = false;
         $scope.creating = false;
     };
+
     $scope.saveQuestion = function () {
         delete $scope.question._body;
         delete $scope.question._type;
         if ($scope.creating) {
-            Questions.create($scope.question);
+            Questions.create($scope.question, function (result) {
+                $scope.question._id = result.id;
+            });
         } else {
             Questions.save($scope.question);
         }
         $scope.editing  = false;
         $scope.creating = false;
     };
-    $scope.deleteQuestion = function () {
-        for (var i = 0; i < $scope.questions.length; i++) {
-            if ($scope.questions[i]._id === $scope.question._id) {
-                Questions.delete($scope.question);
-                $scope.questions.splice(i, 1);
-                break;
+
+    $scope.deleteQuestion = function (question) {
+        if (confirm('Are you sure you want to delete this question?')) {
+            Questions.delete(question);
+            for (var i = 0; i < $scope.questions.length; i++) {
+                if ($scope.questions[i]._id === question._id) {
+                    $scope.questions.splice(i, 1);
+                    $scope.question = null;
+                    break;
+                }
             }
         }
-        $scope.question = null;
-    },
-    $scope.isSelected = function (index) {
-        return ($scope.question &&
-                $scope.question._id ===  $scope.questions[index]._id);
     };
+
+    $scope.isSelected = function (question) {
+        return ($scope.question &&
+                $scope.question._id ===  question._id);
+    };
+
     $scope.proceed = function () {
         var path = $scope.mode === 'assessment' ? 'answer' : 'search'
                  + '/' + $scope.question._id;
